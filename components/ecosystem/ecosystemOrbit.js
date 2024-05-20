3b92cc6c8a19511d0f4879f7fb0f40d92556ff53
eshwarj <eshwarrj@gmail.com>
2024-05-20 16:11:19 +0530

    Animation code refactor

--- components/ecosystem/ecosystemOrbit.js
+++ components/ecosystem/ecosystemOrbit.js
@@ -15,45 +15,50 @@ const EcosystemOrbit = () => {
   return (
     <div className="  -mt-16 relative h-[560px] lg:h-[853px] hidden  lg:flex-center ">
       <div className=" absolute outer-ring-3 flex items-center justify-center animate-spiner  rounded-full">
-        <div className="icon-bg-wp absolute left-[80px] top-[32px]">
-          <Image src={graph} className="    " />
+        <div className="icon-bg-wp absolute left-[80px] top-[32px] animate-spiner-reverse">
+          <Image src={graph} alt="Graph" />
         </div>
-        <div className="icon-bg-wp absolute bottom-[36px] left-[80px]">
-          <Image src={riscZero} className="    " />
+        <div className="icon-bg-wp absolute bottom-[36px] left-[80px] animate-spiner-reverse">
+          <Image src={riscZero} />
         </div>
-        <div className="icon-bg-wp absolute right-[100px] top-[20px]">
-          <Image src={polygon} className="    " />
+        <div className="icon-bg-wp absolute right-[100px] top-[20px] animate-spiner-reverse">
+          <Image src={polygon} alt="riscZero" />
         </div>
-        <div className="icon-bg-wp absolute bottom-[70px] right-[48px]">
-          <Image src={blocksqout} className="    " />
+        <div className="icon-bg-wp absolute bottom-[70px] right-[48px] animate-spiner-reverse">
+          <Image src={blocksqout} />
         </div>
       </div>
-      <div className="absolute outer-ring-2 flex items-center justify-center animate-spiner-reverse !duration-[15s] rounded-full">
-        <div className="icon-bg-wp absolute  top-[-36px]">
-          <Image src={op} className=" " />
+      <div className="absolute outer-ring-2 flex items-center justify-center animate-spiner-reverse  rounded-full">
+        <div className="icon-bg-wp absolute  top-[-36px] animate-spiner">
+          <Image src={op} alt="op" />
         </div>
-        <div className="icon-bg-wp absolute top-[50%]  -right-[30px]">
+        <div className="icon-bg-wp absolute top-[50%]  -right-[30px] animate-spiner">
           <Image src={zkSync} />
         </div>
-        <div className="icon-bg-wp absolute top-[30%]  -left-[30px]">
-          <Image src={espresso} className=" " />
+        <div className="icon-bg-wp absolute top-[30%] -left-[30px] animate-spiner">
+          <Image src={espresso} />
         </div>
-        <div className="icon-bg-wp absolute bottom-[-30px]">
-          <Image src={eigenLayer} className=" " />
+        <div className="icon-bg-wp absolute bottom-[-30px] animate-spiner">
+          <Image src={eigenLayer} alt="espresso" />
         </div>
       </div>
-      <div className="absolute outer-ring-1 flex items-center justify-center animate-spiner !duration-[20s] rounded-full">
-        <div className="icon-bg-wp  absolute top-[20px] -right-[5px]">
-          <Image src={eth} className="" />
+      <div className="absolute outer-ring-1 flex items-center justify-center animate-spiner  rounded-full">
+        <div className="icon-bg-wp  absolute top-[20px] -right-[5px] animate-spiner-reverse">
+          <Image src={eth} alt="eth" />
         </div>
 
-        <div className="icon-bg-wp absolute -left-[20px]   bottom-[30px]">
-          <Image src={arbitrum} className="  " />{" "}
+        <div className="icon-bg-wp absolute -left-[20px] bottom-[30px] animate-spiner-reverse">
+          <Image src={arbitrum} alt="arbitrum" />{" "}
         </div>
       </div>
 
       <div className="absolute rounded-full border-1 border-blue-800 flex items-center justify-center">
-        <Image src={etherscalesymbol} width={150} height={150} />
+        <Image
+          src={etherscalesymbol}
+          alt="etherscalesymbol"
+          width={150}
+          height={150}
+        />
       </div>
     </div>
   );
