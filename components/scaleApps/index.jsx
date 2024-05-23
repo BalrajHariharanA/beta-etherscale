e9329b02277df16a7b5808e5309c0c49896d45b5
eshwarj <eshwarrj@gmail.com>
2024-05-23 17:59:28 +0530

    feedback changes done

--- components/scaleApps/index.jsx
+++ components/scaleApps/index.jsx
@@ -1,96 +1,113 @@
 "use client";
 import scaleAppsFeatures from "@/constants/scaleAppsFeatures";
-import useViewport from '@/hooks/useViewport';
+import { cn } from "@/lib/utils";
+import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
-import React, { useEffect, useRef } from "react";
+import borderRow from "@/public/svgs/borderRow.svg";
+import borderCol from "@/public/svgs/borderCol.svg";
+import useViewport from "@/hooks/useViewport";
 
-const ScaleApps = () => {
-  const spaceHolder = useRef(null);
-  const horizontal = useRef(null);
-  const stickyEle = useRef(null);
-  const {width, height} = useViewport();
+const FeatureCard = ({ feature, featureIdx }) => {
+  return (
+    <div
+      keyref={featureIdx + feature.name}
+      className={cn(
+        "feature-card flex flex-col mb-5 w-full lg:w-1/3 flex-grow-0 flex-shrink-0  group relative p-5 text-center  focus-within:ring-2 focus-within:ring-inset justify-center items-center"
+      )}
+    >
+      <div className="flex items-center justify-center mb-6 rounded-full border-2 border-borderBlue w-[88px] h-[88px]">
+        <div className="flex items-center justify-center rounded-full bg-skyblue-800 w-[74px] h-[74px]">
+          <Image src={feature.icon} alt={feature.name} width={44} height={44} />
+        </div>
+      </div>
+      <div className="w-full flex flex-col justify-center items-center max-lg:mb-6">
+        <h3 className="text-2xl font-bold leading-7 text-white">
+          {feature.name}
+        </h3>
+        <p className="mt-2 text-sm leading-6 text-skyblue-900 lg:line-clamp-3 lg:h-20 max-lg:w-80">
+          {feature.description}
+        </p>
+      </div>
+      <div
+        className={` absolute w-full bottom-0 left-0  h-2  lg:hidden image-bar`}
+      >
+        <Image className="" src={borderRow} fill alt="image" />
+      </div>
+    </div>
+  );
+};
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  function calcDynamicHeight() {
-    const objectWidth = horizontal.current.scrollWidth;
-    return objectWidth - width + height + 10; // 150 is the padding (in pixels) desired on the right side of the .cards container. This can be set to whatever your styles dictate
-  }
+const FeaturesSection = () => {
+  const [elePlacement, setElePlacement] = useState();
+  const eleRef = useRef(null);
+  const { width, height } = useViewport();
+  const additionalContentIndices = Array.from(
+    { length: Math.ceil(scaleAppsFeatures.length / 4) },
+    (_, i) => (i + 1) * 4 - 1
+  );
 
   useEffect(() => {
-    if (spaceHolder.current) {
-      spaceHolder.current.style.height = `${calcDynamicHeight()}px`;
-    }
-
-    const handleScroll = () => {
-      if (horizontal.current && stickyEle.current) {
-        horizontal.current.style.transform = `translateX(-${stickyEle.current.offsetTop}px)`;
-      }
-    };
-
-    const handleResize = () => {
-      if (spaceHolder.current) {
-        spaceHolder.current.style.height = `${calcDynamicHeight()}px`;
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-      window.removeEventListener("resize", handleResize);
-    };
-  }, [calcDynamicHeight]);
+    const Eleheight = eleRef ? eleRef.current?.clientHeight : "";
+    setElePlacement(
+      Math.ceil(Eleheight / (additionalContentIndices.length + 1))
+    );
+  }, [width, height, additionalContentIndices]);
 
   return (
-    <section className=" w-screen">
-      <div ref={spaceHolder} className="space-holder">
-        <div ref={stickyEle} className="sticky-box">
-          <div className="max-w-2xl px-4 pt-8 mx-auto text-center heading-cards">
-            <h2 className="mt-2 text-white heading-bold">
-              Make ScaleApps life easier{" "}
-            </h2>
-            <p className="mt-6 text-center text-medium text-skyblue-90">
-            Explore EtherScale’s additional features that support the deployment, operation, and management of ScaleApps, enhancing overall functionality and user experience.
-            </p>
+    <section className="w-screen">
+      <div className="space-holder lg:border-borderBlue lg:border lg:rounded-[40px] m-auto max-w-[1072px] my-16">
+        <div className="max-w-2xl px-4 pt-8 mx-auto text-center heading-cards mb-5 lg:mb-8">
+          <h2 className="mt-2 text-white heading-bold">
+            Make ScaleApps life easier{" "}
+          </h2>
+          <p className="mt-6 text-center text-medium text-skyblue-90">
+            Explore EtherScale’s additional features that support the
+            deployment, operation, and management of ScaleApps, enhancing
+            overall functionality and user experience.
+          </p>
+        </div>
+        <div className="horizontal  lg:hidden ">
+          <div
+            ref={eleRef}
+            className=" flex  relative  flex-wrap  overflow-hidden  shadow "
+          >
+            {scaleAppsFeatures.map((feature, featureIdx) => (
+              <FeatureCard
+                key={featureIdx}
+                feature={feature}
+                featureIdx={featureIdx}
+              />
+            ))}
           </div>
-          <div ref={horizontal} className="horizontal">
-            <section role="feed" className="scrolling-cards-wp mt-20 container">
-              {scaleAppsFeatures.map((feature) => (
-                <div key={feature[0].name} className="max-md:flex">
-                <div
-                  className="flex flex-col gap-5 w-[365px] sm:w-[450px] flex-grow-0 flex-shrink-0  p-10 transition-all duration-500"
-                >
-                  <dt className="text-2xl font-bold leading-7 text-white ">
-                    <div className="flex items-center justify-center mb-6 rounded-full border-2 border-borderBlue w-[88px] h-[88px]">
-                      <div className="flex items-center justify-center rounded-full bg-skyblue-800 w-[74px] h-[74px]">
-                        <Image src={feature[0].icon} alt={feature[0].name}  width={44} height={44}/>
-                      </div>
-                    </div>
-                    {feature[0].name}
-                  </dt>
-                  <dd className="flex flex-col flex-auto mt-1 text-sm leading-7 text-skyblue-900">
-                    <p className="flex-auto">{feature[0].description}</p>
-                  </dd>
-                </div>
-                <div
-                  className="flex flex-col gap-5 w-[365px] sm:w-[450px] flex-grow-0 flex-shrink-0  p-10 transition-all duration-500"
-                >
-                  <dt className="text-2xl font-bold leading-7 text-white ">
-                    <div className="flex items-center justify-center mb-6 rounded-full border-2 border-borderBlue w-[88px] h-[88px]">
-                      <div className="flex items-center justify-center rounded-full bg-skyblue-800 w-[74px] h-[74px]">
-                        <Image src={feature[1].icon} alt={feature[1].name}  width={44} height={44}/>
-                      </div>
-                    </div>
-                    {feature[1].name}
-                  </dt>
-                  <dd className="flex flex-col flex-auto mt-1 text-sm leading-7 text-skyblue-900">
-                    <p className="flex-auto">{feature[1].description}</p>
-                  </dd>
-                </div>
+        </div>
+
+        <div className="horizontal hidden lg:block  ">
+          <div
+            ref={eleRef}
+            className=" flex  relative  flex-wrap  overflow-hidden  shadow "
+          >
+            <div className="absolute top-0 left-[33%] right-0 h-full w-3">
+              <Image className="" src={borderCol} fill alt="image" />
+            </div>
+            <div className="absolute top-0 left-[66%] right-0 h-full w-3">
+              <Image className="" src={borderCol} fill alt="image" />
+            </div>
+            {scaleAppsFeatures.map((feature, featureIdx) => (
+              <FeatureCard
+                key={feature.name + featureIdx}
+                feature={feature}
+                featureIdx={featureIdx}
+              />
+            ))}
+            {additionalContentIndices.map((item, index) => (
+              <div
+                key={item + index}
+                className={`absolute top-0 left-0 right-0 h-2 `}
+                style={{ top: `${elePlacement * (index + 1) - 20}px` }}
+              >
+                <Image className="" src={borderRow} fill alt="image" />
               </div>
-              ))}
-            </section>
+            ))}
           </div>
         </div>
       </div>
@@ -98,4 +115,4 @@ const ScaleApps = () => {
   );
 };
 
-export default ScaleApps;
+export default FeaturesSection;
--- constants/scaleAppsFeatures.js
+++ constants/scaleAppsFeatures.js
@@ -11,84 +11,94 @@ import Staking from "@/public/images/staking.png";
 import TestNetSupport from "@/public/images/faucet.png";
 import Support from "@/public/images/support.png";
 const scaleAppsFeatures = [
-  [
   {
     name: "Indexers",
-    description: "Improve data query efficiency across the platform to boost ScaleApps performance.",
+    description:
+      "Improve data query efficiency across the platform to boost ScaleApps performance.",
     href: "#",
     icon: Indexer,
   },
   {
     name: "Smart Contract Toolkit",
-    description: " Pre-configured smart contract templates and development tools customized for ScaleApps.",
+    description:
+      " Pre-configured smart contract templates and development tools customized for ScaleApps.",
     href: "#",
     icon: Settings,
   },
-],[
+
   {
     name: "Data Availability",
-    description: "Guarantee continuous availability and security of vital data for all network participants.",
+    description:
+      "Guarantee continuous availability and security of vital data for all network participants.",
     href: "#",
     icon: DataAvailability,
   },
   {
     name: "Cross-rollup Messaging",
-    description: "Enable seamless communication between ScaleApps for interoperability.",
+    description:
+      "Enable seamless communication between ScaleApps for interoperability.",
     href: "#",
     icon: CrossRollup,
   },
-],[
+
   {
     name: "Oracle Services",
-    description: "Provide real-time data to smart contracts for dynamic DApp functions.",
+    description:
+      "Provide real-time data to smart contracts for dynamic DApp functions.",
     href: "#",
     icon: Cloud,
   },
   {
     name: "Wallet Service",
-    description: "Integrate wallet with MPC services for streamlined asset management in ScaleApps.",
+    description:
+      "Integrate wallet with MPC services for streamlined asset management in ScaleApps.",
     href: "#",
     icon: Wallet,
   },
-],[
+
   {
     name: "Explorer",
-    description: "Offer a holistic view of network transactions to enhance transparency.",
+    description:
+      "Offer a holistic view of network transactions to enhance transparency.",
     href: "#",
     icon: Explorer,
   },
   {
     name: "Testnet Faucets",
-    description: "Supply developers with test tokens for DApp testing in sandbox environments.",
+    description:
+      "Supply developers with test tokens for DApp testing in sandbox environments.",
     href: "#",
     icon: TestNetSupport,
   },
-],[
+
   {
     name: "Native Stable Coins",
-    description: "Incorporate native stable coins within ScaleApps to streamline stable and secure transactions.",
+    description:
+      "Incorporate native stable coins within ScaleApps to streamline stable and secure transactions.",
     href: "#",
     icon: NativeStable,
   },
   {
     name: "Staking for ScaleApps",
-    description: "Allow token staking within ScaleApps for network security and reward earning..",
+    description:
+      "Allow token staking within ScaleApps for network security and reward earning..",
     href: "#",
     icon: Staking,
   },
-],[
+
   {
     name: "Automatic Monitoring",
-    description: "Monitor network conditions and DApp performance, delivering automated alerts and analytics.",
+    description:
+      "Monitor network conditions and DApp performance, delivering automated alerts and analytics.",
     href: "#",
     icon: Automatic,
   },
   {
     name: "Support",
-    description: "Offer strong technical support and engage with the community to aid developers and users.",
+    description:
+      "Offer strong technical support and engage with the community to aid developers and users.",
     href: "#",
     icon: Support,
   },
-],
 ];
 export default scaleAppsFeatures;
